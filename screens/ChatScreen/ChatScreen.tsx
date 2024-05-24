1ccd24a497412e49d6557a5e63236f2b5b48a007
asifrahaman13 <asifrahaman162@gmail.com>
2024-05-24 22:30:06 +0530

    wip: integrated the ui with the states for the conersation

--- screens/ChatScreen/ChatScreen.tsx
+++ screens/ChatScreen/ChatScreen.tsx
@@ -1,5 +1,12 @@
 import React, {useState, useEffect, useRef} from 'react';
-import {Button, StyleSheet, Text, View, TextInput} from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  ScrollView,
+  TouchableOpacity,
+} from 'react-native';
 // import {useDispatch, useSelector} from 'react-redux';
 
 import {WEBSOCKET_URI} from '@env';
@@ -9,8 +16,8 @@ export default function ChatScreen({navigation}: any) {
   const [message, setMessage] = useState('');
   const [messages, setMessages] = useState([
     {
-      type: 'client',
-      message: 'Hello',
+      type: null,
+      message: null,
     },
   ]);
   const websocketRef = useRef<WebSocket | null>(null);
@@ -59,32 +66,41 @@ export default function ChatScreen({navigation}: any) {
     }
     setMessages(prevMessages => [
       ...prevMessages,
-      {type: 'server', message: message},
+      {type: 'client', message: message},
     ]);
   };
 
   return (
     <View style={styles.container}>
-      {messages?.map((item, index) => (
-        <>
-          <View>
-            <Text key={index} style={styles.header}>
-              {item?.message}
-            </Text>
-          </View>
-        </>
-      ))}
-      <TextInput
-        style={styles.input}
-        value={message}
-        onChangeText={setMessage}
-        placeholder="Type a message"
-      />
-      <Button title="Send" onPress={sendMessage} />
-      {/* <Button
-        title="Go to Home page... again"
-        onPress={() => navigation.navigate('Home')}
-      /> */}
+      <ScrollView style={styles.messagesContainer}>
+        {messages.map((item, index) => (
+          <>
+            {item.type && (
+              <View
+                key={index}
+                style={[
+                  styles.messageBubble,
+                  item.type === 'client'
+                    ? styles.clientMessage
+                    : styles.serverMessage,
+                ]}>
+                <Text style={styles.messageText}>{item.message}</Text>
+              </View>
+            )}
+          </>
+        ))}
+      </ScrollView>
+      <View style={styles.inputContainer}>
+        <TextInput
+          style={styles.input}
+          value={message}
+          onChangeText={setMessage}
+          placeholder="Type a message"
+        />
+        <TouchableOpacity onPress={sendMessage} style={styles.button}>
+          <Text style={styles.messageText}>Send</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
@@ -92,44 +108,50 @@ export default function ChatScreen({navigation}: any) {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'flex-end',
-    alignItems: 'center',
+    backgroundColor: '#f5f5f5',
     padding: 10,
   },
-  chatList: {
+  messagesContainer: {
     flex: 1,
-    width: '100%',
-    color: 'black',
   },
-  messageContainer: {
-    backgroundColor: '#f1f1f1',
+  messageBubble: {
     padding: 10,
-    borderRadius: 5,
     marginVertical: 5,
+    borderRadius: 10,
+    maxWidth: '80%',
+  },
+  clientMessage: {
+    backgroundColor: '#FB9980',
+    alignSelf: 'flex-end',
+  },
+  serverMessage: {
+    backgroundColor: '#4DD0BC',
+    alignSelf: 'flex-start',
   },
   messageText: {
     fontSize: 16,
+    color: 'white',
+  },
+  inputContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    padding: 10,
+    borderTopWidth: 1,
+    borderTopColor: '#ECECEC',
   },
   input: {
-    height: 40,
-    borderColor: 'gray',
+    flex: 1,
+    padding: 10,
     borderWidth: 1,
-    borderRadius: 5,
-    paddingHorizontal: 10,
-    marginBottom: 10,
-    width: '100%',
-  },
-  counterContainer: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center',
-    width: '100%',
-  },
-  count: {
-    fontSize: 30,
-  },
-  header: {
-    fontSize: 20,
+    borderColor: '#ECECEC',
+    borderRadius: 20,
+    marginRight: 10,
+    backgroundColor: '#FFF',
     color: 'black',
+  },
+  button: {
+    padding: 10,
+    borderRadius: 6,
+    backgroundColor: '#4DD0BC',
   },
 });
